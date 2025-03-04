580b21a725059585d54a9d23960f0ec2a87f76b6
Ian-q <ianadelman@outlook.com>
2025-03-04 00:56:07 -0800

    completely re-wrote robot_draw

--- MATLAB/robot_draw.m
+++ MATLAB/robot_draw.m
@@ -1,76 +1,67 @@
 function robot_draw(gamma, fig, frameNum)
-    % Draw the robot in 3D for given joint parameters
-    % gamma: a vector of joint angles
+% Draw the robot in 3D for given joint angles with coordinate frames
+% gamma: a vector of joint angles
 
-    persistent p
+persistent p
 
-    if nargin < 1
-        gamma = zeros(1,5);
-    end
-
-    if nargin < 2
-        fig = gcf;
-        frameNum = 1;
-        camlight;
-    end
-
-    % Compute the forward kinematics
-    T = robot_FK(gamma);
-
-    % Extract the positions of the frames
-    num_joints = length(gamma);
-    positions = zeros(3, num_joints + 1);
-    positions(:, 1) = [0; 0; 0]; % Base frame position
-
-    % Extract positions from the transformation matrices
-    for i = 1:num_joints
-        T_matrix = T{:, i};
-        positions(:, i + 1) = T_matrix(1:3, 4);
-    end
-
-    axis_scale = 1;
-    if frameNum < 2
-        % Set up figure
-        fig = gcf;
-        % axis([-1, 1, -1, 1, 0, 1.7]*axis_scale);
-        xlabel('X');
-        ylabel('Y');
-        zlabel('Z');
-        title('Robot 3D Visualization');
-        view([1,1,1]);
-        axis equal;
-        camlight left;
-
-        % Plot the links
-        hold on;
-        grid on;
-        p.links = gobjects(1, num_joints);
-        for i = 1:num_joints
-            p.links(i) = plot3([positions(1, i), positions(1, i+1)], ...
-                               [positions(2, i), positions(2, i+1)], ...
-                               [positions(3, i), positions(3, i+1)], 'b-', 'LineWidth', 2);
-        end
-
-        % Plot the frames
-        p.frames = gobjects(1, num_joints + 1);
-        for i = 1:num_joints + 1
-            p.frames(i) = plot3(positions(1, i), positions(2, i), positions(3, i), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
-        end
-        hold off;
-    else
-        % Update the links
-        for i = 1:num_joints
-            set(p.links(i), 'XData', [positions(1, i), positions(1, i+1)], ...
-                            'YData', [positions(2, i), positions(2, i+1)], ...
-                            'ZData', [positions(3, i), positions(3, i+1)]);
-        end
-
-        % Update the frames
-        for i = 1:num_joints + 1
-            set(p.frames(i), 'XData', positions(1, i), ...
-                             'YData', positions(2, i), ...
-                             'ZData', positions(3, i));
-        end
-    end
-    axis([-1, 1, -1, 1, -.5, 1.7]*axis_scale);
+if nargin < 1
+    gamma = zeros(1,5);
 end
+
+if nargin < 2
+    fig = figure();
+    frameNum = 1;
+    camlight;
+end
+
+% Compute the forward kinematics
+T = robot_FK(gamma);
+
+r_00r1 = T.("T_0T1")(1:3,4);
+r_00r2 = T.("T_0T1")(1:3,4);
+r_00r3 = T.("T_0T2")(1:3,4);
+r_00r4 = T.("T_0T3")(1:3,4);
+r_00r5 = T.("T_0T4")(1:3,4);
+r_00r6 = T.("T_0T5")(1:3,4);
+
+
+line01 = line('Xdata', [0, r_00r1(1)],...
+              'Ydata', [0, r_00r1(2)],...
+              'Zdata', [0, r_00r1(3)],...
+              'color', 'red');
+
+line12 = line('Xdata', [r_00r1(1), r_00r2(1)],...
+              'Ydata', [r_00r1(2), r_00r2(2)],...
+              'Zdata', [r_00r1(3), r_00r2(3)],...
+              'color', 'black');
+
+line23 = line('Xdata', [r_00r2(1), r_00r3(1)],...
+              'Ydata', [r_00r2(2), r_00r3(2)],...
+              'Zdata', [r_00r2(3), r_00r3(3)],...
+              'color', 'red');
+
+line34 = line('Xdata', [r_00r3(1), r_00r4(1)],...
+              'Ydata', [r_00r3(2), r_00r4(2)],...
+              'Zdata', [r_00r3(3), r_00r4(3)],...
+              'color', 'black');
+
+line45 = line('Xdata', [r_00r4(1), r_00r5(1)],...
+              'Ydata', [r_00r4(2), r_00r5(2)],...
+              'Zdata', [r_00r4(3), r_00r5(3)],...
+              'color', 'red');
+
+line56 = line('Xdata', [r_00r5(1), r_00r6(1)],...
+              'Ydata', [r_00r5(2), r_00r6(2)],...
+              'Zdata', [r_00r5(3), r_00r6(3)],...
+              'color', 'black');
+
+
+
+
+
+figure(fig)
+view(3)
+grid on
+axis equal
+xlabel('X');ylabel('Y');zlabel('Z');
+axis([-1,1, -1,1, -1,1]*3);
