982ee55cc9a0268088494e09f17a6dd0019290ed
Ian-q <ianadelman@outlook.com>
2025-03-12 17:14:22 -0700

    remove extra lines

--- MATLAB/robot_IK_test.m
+++ MATLAB/robot_IK_test.m
@@ -3,7 +3,7 @@ clear
 clc
 
 
-input_joint_angles = ones(5,1)*pi/2;
+input_joint_angles = ones(5,1)*.1;
 T = robot_FK(input_joint_angles);
 T_0T6 = T.("T_0T6");
 pos_desired = T_0T6(1:3,4);
@@ -15,13 +15,3 @@ q0 = zeros(5,1);
 
 q = robot_IK(pos_desired, q0)
 
-
-
-
-q =
-
-  -83.2522
-   16.4818
-   84.0491
-         0
-         0
