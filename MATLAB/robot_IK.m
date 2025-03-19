bc7e6e29e6d15f37e41e5ed1e9a85c9c4016ab00
Ian-q <ianadelman@outlook.com>
2025-03-19 08:30:11 -0700

    Update robot_IK.m
    add option to not display the output from the numerical IK calculation

--- MATLAB/robot_IK.m
+++ MATLAB/robot_IK.m
@@ -1,6 +1,10 @@
-function joint_angles = robot_IK(desired_pos, q0)
+function joint_angles = robot_IK(desired_pos, q0, display_on)
     % q0 is current state
     % desired_pos is the desired end effector position
+
+    if nargin < 3
+        display_on = true;
+    end
     
     % Inverse kinematics using optimization
     
@@ -8,7 +12,12 @@ function joint_angles = robot_IK(desired_pos, q0)
     objective = @(q) norm(robot_FK(q).("T_0T6")(1:3, 4) - desired_pos);
     
     % Optimization options
-    options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
+    if display_on
+        options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
+    else
+        % Turn off display if user doesnt want it
+        options = optimoptions('fmincon', 'Display', 'off', 'Algorithm', 'sqp');
+    end
 
     % Solve using fmincon (can also try lsqnonlin or fminunc)
     joint_angles = fmincon(objective, q0, [], [], [], [], [], [], [], options);
